37498c90ed2e28d25bb4f1d2dc22354285bceefa
JRsFly <58620798+JRsFly@users.noreply.github.com>
2021-12-24 19:15:34 +0800

    Update domtree.js

--- domtree/js/domtree.js
+++ domtree/js/domtree.js
@@ -2,7 +2,7 @@ const width = 600;
 const height = 550;
 const quantity = 170;
 const types = [ 'text', 'select', 'progress', 'meter', 'button', 'radio', 'checkbox' ];
-const greetings = [ '祝大家节日快乐','圣诞节快乐','每天都开开心心','祝代码顺利编译无bug','编译无bug','圣诞节快乐','祝收到圣诞老人的礼物','节日快乐','玩得开心','拒绝bug','生活愉快', '圣诞节快乐','节日快乐','祝一切顺利','节日的问候','心想事成','生活愉快','工作顺利' ];
+const greetings = [ '祝大家节日快乐','圣诞节快乐','每天都开开心心','祝代码顺利编译无bug','JRsFly2017','圣诞节快乐','玩得开心','拒绝bug','生活愉快', '圣诞节快乐','节日快乐','祝一切顺利','节日的问候','心想事成','生活愉快','考研顺利' ];
 
 let tree = document.querySelector( '.tree' ),
 treeRotation = 0;
